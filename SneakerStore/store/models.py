ea42580285233789ef5a5702558c13b84961dea4
salvadoriscte <svmsa3@iscte-iul.pt>
2023-04-08 17:24:04 +0100

    Primeiro commit

---
+++ SneakerStore/store/models.py
@@ -0,0 +1,61 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Cliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    morada = models.CharField(max_length=255)
+    telemovel = models.IntegerField()
+    nif = models.IntegerField()
+    imagem = models.ImageField(upload_to='clientes/')
+    estilo_preferido = models.ForeignKey('Estilo', on_delete=models.SET_NULL, null=True)
+    tamanho_preferido = models.ForeignKey('Tamanho', on_delete=models.SET_NULL, null=True)
+
+class PreferenciaMarca(models.Model):git push -u origin main
+
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    marca = models.ForeignKey('Marca', on_delete=models.CASCADE)
+
+class EmpregadoLoja(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    morada = models.CharField(max_length=255)
+    nif = models.IntegerField()
+    telemovel = models.IntegerField()
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=255)
+    pais = models.CharField(max_length=255)
+
+class Tamanho(models.Model):
+    tamanho = models.CharField(max_length=50)
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+class Sneaker(models.Model):
+    nome = models.CharField(max_length=255)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    estilo = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    tamanho = models.ForeignKey(Tamanho, on_delete=models.CASCADE)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    imagem = models.ImageField(upload_to='sneakers/')
+    quantidade_stock = models.IntegerField()
+
+class Carrinho(models.Model):
+    cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE)
+    ativo = models.BooleanField()
+    data_inatividade = models.DateField()
+
+class ItemCarrinho(models.Model):
+    sneaker = models.ForeignKey(Sneaker, on_delete=models.CASCADE)
+    carrinho = models.ForeignKey(Carrinho, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+
+class Encomenda(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data = models.DateTimeField()
+    status = models.CharField(max_length=255)
+
+class ItemEncomenda(models.Model):
+    encomenda = models.ForeignKey(Encomenda, on_delete=models.CASCADE)
+    sneaker = models.ForeignKey(Sneaker, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
